5f94446f023bcc7cdebde41e7cd6b6adbc828cef
CorruptedArk <noahstandingford@gmail.com>
2015-06-10 14:55:04 -0400

    SafetyBot changes again.

--- SafetyBot/src/org/usfirst/frc/team1528/robot/Robot.java
+++ SafetyBot/src/org/usfirst/frc/team1528/robot/Robot.java
@@ -3,7 +3,8 @@ package org.usfirst.frc.team1528.robot;
 
 
 import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.smartdashboard.*;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 /**
  * This is a demo program showing the use of the RobotDrive class.
  * The SampleRobot class is the base of a robot application that will automatically call your
@@ -23,10 +24,14 @@ import edu.wpi.first.wpilibj.Relay;
 public class Robot extends SampleRobot {
     RobotDrive safteyDrive;
     Joystick safteyController;
-    Talon safteyMotor, safteyMotor2;
+    Talon safteyMotor;
     DigitalInput safteySwitch;
-    DoubleSolenoid safetyPiston,piston;
-    Relay safetyRelay;
+    //DoubleSolenoid safetyPiston;
+    //Relay safetyRelay;
+    
+    SendableChooser autoPicker;
+    SendableChooser telePicker;
+    
   //Constants for Buttons
     static final int A_BUTTON = 1;
     static final int B_BUTTON = 2;
@@ -50,32 +55,67 @@ public class Robot extends SampleRobot {
     
     
     public Robot() {
-        safteyDrive = new RobotDrive(0,1,2,3);
+    	
+        safteyDrive = new RobotDrive(5,0,3,2);
         safteyController = new Joystick(0);
         safteySwitch = new DigitalInput(0);
-        safetyPiston = new DoubleSolenoid(0,1);
-        safetyRelay = new Relay(0);
-        safteyMotor2 = new Talon(5);
-        safteyMotor = new Talon(4);
+        //safetyPiston = new DoubleSolenoid(0,1);
+        //safetyRelay = new Relay(0);
+        safteyMotor = new Talon(1);
+        
+        autoPicker = new SendableChooser();
+        telePicker = new SendableChooser();
+        autoPicker.addDefault("Auto 0", new Integer(0));
+        autoPicker.addObject("Auto 1", new Integer(1));
+        telePicker.addDefault("Tele 0", new Integer(0));
+        telePicker.addObject("Tele 1", new Integer(1));
+        SmartDashboard.putData("Auto Picker", autoPicker);
+        SmartDashboard.putData("Tele Picker", telePicker);
+        
+        safteyDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+        safteyDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
     } 
 
     
     public void autonomous() {
-    	/*
-    	safteyDrive.mecanumDrive_Cartesian(1 , 0, 1, 00);
-        Timer.delay(666);  */
+    	switch(((Integer)autoPicker.getSelected()).intValue()){
+    		case 0:
+    			auto0();
+    			break;
+    		case 1:
+    			auto1();
+    			break;
+    	}
         
     }
 
+    public void auto0(){
+    	
+    }
+    
+    
+    public void auto1(){
+    	
+    }
+    
     
     public void operatorControl() {
-    	while(isOperatorControl() && isEnabled()){
     	
+    	switch(((Integer)telePicker.getSelected()).intValue()){
+    		case 0:
+    			teleOp0();
+    			break;
+    		case 1:
+    			teleOp1();
+    			break;
+    	}
+    		
+    }
+    	
+    public void teleOp0(){
+    	while(isOperatorControl() && isEnabled()){
+        	
     		safteyMotor.setSafetyEnabled(true);
-    		safteyMotor2.setSafetyEnabled(true);
-    		
-    		
-    		
     		
     		safteyDrive.setSafetyEnabled(true);
     		double x = buffer(LEFT_X_AXIS,safteyController,true,0.18,-0.18);
@@ -83,22 +123,31 @@ public class Robot extends SampleRobot {
     		double r = buffer(RIGHT_X_AXIS, safteyController,true,0.18, -0.18);
     		safteyDrive.mecanumDrive_Cartesian(x, y, r, 0.0);
     		
-    		safteyMotor.set(buffer(LEFT_TRIGGER_AXIS,safteyController,true,0.18,-0.18));
-    		safteyMotor2.set(buffer(RIGHT_TRIGGER_AXIS,safteyController,true,0.18,-0.18));
+    		/*if(safteyController.getRawButton(A_BUTTON) && safteyController.getRawButton(B_BUTTON)){
+    		}else if(safteyController.getRawButton(A_BUTTON)){
+    			 safetyPiston.set(Value.kForward);
+    		}else if(safteyController.getRawButton(B_BUTTON)){
+    			safetyPiston.set(Value.kReverse);
+    		}*/
+    		
+    		
     		
     		/*if(safteyController.getRawButton(B_BUTTON)){
     			safteyDrive.mecanumDrive_Cartesian(0, 0, 1.0, 0);
     			Timer.delay(5.0);
     			safteyDrive.mecanumDrive_Cartesian(0, 0, 0, 0);	
-    		}
+    		}*/
     			
+    		double left = buffer(LEFT_TRIGGER_AXIS, safteyController,false,0.2, -0.2);
+    		double right = buffer(RIGHT_TRIGGER_AXIS, safteyController,true,0.2, -0.2);
+    		
     		if(!safteySwitch.get()){
-    			safteyMotor.set(buffer(RIGHT_Y_AXIS, safteyController,true,0.18, -0.18)*.4000000000);
+    			safteyMotor.set((left+right)*.4000000000);
     		}else{
-    			safteyMotor.set(buffer(RIGHT_X_AXIS, safteyController,true,0.18, -0.18)*1.00);
+    			safteyMotor.set((left+right)*1.00);
     		}
     		
-    		if(safteyController.getRawButton(A_BUTTON)){
+    		/*if(safteyController.getRawButton(A_BUTTON)){
     			safetyRelay.set(Relay.Value.kForward);
     		}else{
     			safetyRelay.set(Relay.Value.kOff);
@@ -108,12 +157,20 @@ public class Robot extends SampleRobot {
     			
     		Timer.delay(0.01);
     	}
-    		
-    		
     }
-    	
     
-
+    
+    
+    public void teleOp1(){
+    	while(isOperatorControl() && isEnabled()){
+    		
+    		safteyDrive.setSafetyEnabled(true);
+    		double x = buffer(LEFT_X_AXIS,safteyController,true,0.18,-0.18);
+    		double y = buffer(LEFT_Y_AXIS, safteyController,true,0.18,-0.18);
+    		double r = buffer(RIGHT_X_AXIS, safteyController,true,0.18, -0.18);
+    		safteyDrive.mecanumDrive_Cartesian(x, y, r, 0.0);
+    	}
+    }
     /**
      * Runs during test mode
      */
